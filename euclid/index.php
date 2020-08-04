645a7f29ba94e2f76a4a9bc5248fda048fce38da
Erin Bell <ebellempire@gmail.com>
2020-08-04 10:37:08 -0400

    minor

--- euclid/index.php
+++ euclid/index.php
@@ -22,7 +22,13 @@ include '../common/header.php';
 		<figcaption>Fig1: Interactive map displaying historical city directory data for lower Euclid Avenue in Cleveland, Ohio (Public Square to East 9th Street). Source: Cleveland City Directories, select years from 1937 to 1997. <em><strong>NOTE:</strong> 2019 data was manually compiled by Mark Souther and is not part of any city directory.</em> <a download class="data-download" href="euclid.csv">Download source data as .csv file</a>
 		</figcaption>
 	</figure>
+	<section class="explanation">
 	<p>City directories were compendia of residents, businesses, and institutions with associated addresses that were widely used in U.S. cities from the 19th century into the 1970s (and sporadically thereafter). Beginning in 1929, city directories incorporated a new feature, the criss-cross directory, which listed all building occupants by street and then by address, enabling them to be read in a place-based manner. Commercial listings also included abbreviated business types. Although these varied from year to year, we have devised a typology of categories and subcategories (adopting widely recognized terminology) that enable more meaningful and accurate analysis. City directories are a rich resource for spatial analysis that can inform urban, public, and digital history. This visualization depicts nearly a century of change along lower Euclid Avenue, the spine of downtown Cleveland.</p>
+	</section>
+	<section class="chart-canvas">
+	</section>
+	<section class="table-canvas">
+	</section>
 </section>
 
 <?php
